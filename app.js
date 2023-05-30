f8d6c3c3615c74505a4b5ac53166144c50b9a0ed
richfloww <richflow678@yahoo.com>
2023-05-30 07:47:47 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+console.log("hello world");
+console.log("welcome to class");
+
+// // JS VARIABLES AND VALUES (var, let & const)
+//  let x = 2;
+//  x=9;
+//  const y = 4;
+//  console.log(x);
+//  console.log(y);
+// //  CONVENTIONS AND RULES IN NAMING VARIABLES
+// // HAVE A LOGICAL NAME
+// // const B = "rICHFLOW"
+// // const firstname = "Richflow"
+
+
+// // const country = "Nigeria"
+// // let state = "Lagos"
+// // console.log(country);
+// // console.log(state);
+// // state = "Kaduna"
+// // console.log(state);
+// // js DATA TYPE - ()
+// const firstName = "John"
+// const  lastName = "Doe"
+// const middleName = "Owen"
+// const currentJob = "Product Manager"
+// const year = 2002
+// const email = 'richflow@techstudioacademy.com';
+// const fullName = firstName + " " + middleName + " " + lastName
+// console.log(fullName);
+// // my nam is john and i work as a product manager
+// const description = " my name is " + firstName + " and i work as a " + currentJob
+// console.log(description);
+
+// // you can message me on this email, Richflow@techstudio.com
+
+// const narrative = " you can message me on this email, " + email 
+// console.log(narrative);
+// // getting charcauy7
+
+
+
+// // extracting parts of a string - slice substring, substr
+
+// const email2 = "richflow678@techstudio.com";
+// // string
+// console.log(email.slice[5, 12])
+// console.log(email.slice[-5, -1]);
+// console.log();
+
+
+
+// console.log(email2.includes("mubar"));
+// console.log(email.indexOf("a"));
+// console.log(email.lastIndexOf("a"));
+// const user
+
+const author = "chinua Achebe";
+const book = " Things Fall Apart";
+const year1 = 1996;
+ const narr = " the  book " + book + " was written by " + author + " in the year " + year1;
+
+ console.log("narr");jlj
